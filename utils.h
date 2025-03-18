7781e6ff71b6d583036a5600a77766a245e8bce3
Ahmed-Elnaggar1 <ahmed.aly.elnaggar1@gmail.com>
2025-03-18 14:51:33 +0200

    expedia

---
+++ utils.h
@@ -0,0 +1,47 @@
+#ifndef EXPEDIA_COM_UTILS_H
+#define EXPEDIA_COM_UTILS_H
+
+#include<chrono> // for date and time src : https://stackoverflow.com/questions/17223096/outputting-date-and-time-in-c-using-stdchrono
+#include<iomanip>
+#include<cassert>
+using namespace std;
+
+// some utilities for the project
+int ReadInt(int min, int max, bool cancel_choice_allowed = false) {
+    if (!cancel_choice_allowed)
+        cout << "\nEnter number in range " << min << " - " << max << ": ";
+    else
+        cout << "\nEnter -1 to cancel or number in range " << min << " - " << max << ": ";
+
+    int val;
+    cin >> val;
+
+    if (cancel_choice_allowed && val == -1)
+        return val;
+
+    if (min <= val && val <= max)
+        return val;
+
+    cout << "ERROR: invalid number...Try again\n";
+    return ReadInt(min, max);
+}
+
+int ShowReadMenu(const vector<string> &choices, string header = "Menu") {
+    cout << "\n"<<header<<":\n";
+    for (int ch = 0; ch < (int) choices.size(); ++ch) {
+        cout << "\t" << ch + 1 << ": " << choices[ch] << "\n";
+    }
+    return ReadInt(1, choices.size());
+}
+
+// get current time and date
+string GetCurrentTimeDate() {
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+    return ss.str();
+}
+
+#endif //EXPEDIA_COM_UTILS_H
